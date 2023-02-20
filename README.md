45e00bbe8c41a87b4a14da4316d77f1fc215b368
Matthieu Imbert <matthieu.imbert@inria.fr>
2023-02-20 10:37:11 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# gst-playbin-seek-test
+
+Plays a sound with a fast attack with gstreamer playbin. Check
+behavior in VMs debian bullseye with pulseaudio and debien testing
+with pipewire.
+
+Dependencies: vagrant (virtualbox)
+
+Usage:
+
+- build the VMS
+
+    $ vagrant up
+
+- connect to a VM and run the test
+
+    $ vagrant ssh <debstable|debtesting>
+    vagrant$ cd /vagrant
+    vagrant$ ./gst-playbin-seek-test.py
---
+++ gst-playbin-seek-test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import gi, os.path, pathlib, time
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, Gtk, GLib
+
+if __name__ == '__main__':
+    Gst.init(None)
+    player = Gst.ElementFactory.make('playbin')
+    fakevideosink = Gst.ElementFactory.make('fakesink')
+    player.set_property("video-sink", fakevideosink)
+    uri = pathlib.Path(os.path.abspath("./bd_heavy.wav")).as_uri()
+    player.set_property('uri', uri)
+
+    while True:
+        player.set_state(Gst.State.PAUSED)
+        player.seek(1.0,
+                    Gst.Format.TIME,
+                    Gst.SeekFlags.FLUSH,
+                    Gst.SeekType.SET, 0,
+                    Gst.SeekType.NONE, 0)
+        player.set_state(Gst.State.PLAYING)
+        while True:
+            got_duration, duration = player.query_duration(Gst.Format.TIME)
+            got_position, position = player.query_position(Gst.Format.TIME)
+            print(f"position={position} duration={duration}")
+            if got_duration and got_position:
+                if position >= duration:
+                    print(f"reached end")
+                    break
+            time.sleep(0.1)
+        time.sleep(2) # wait 2 seconds before replaying the sound
